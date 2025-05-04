abfe533b78ac8fceea1ab4e1cb46fe2653b6e3fa
JayangaPabasara <jayangapabasara71@gmail.com>
2025-05-04 14:04:58 +0530

    Add Cors configuration

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnection from "./config/dbConnection.js";
 import userRoute from "./routes/userRoutes.js";
-import bodypracer from "body-parser"
+import bodypracer from "body-parser";
 import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(bodypracer.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://country-api-frontend-eight.vercel.app/",
+    credentials: true,
+  })
+);
 
 dbConnection(process.env.MONGO_DB_URI);
 
@@ -17,6 +22,6 @@ app.use("/api/user", userRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () =>{
-    console.log(`Server is running on ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
